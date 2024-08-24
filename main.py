7d546b9da1fdc62627ef3c93e35eb41ad884e64a
Kaan Emir Çetin <kaanemircetin1@gmail.com>
2024-08-25 00:29:12 +0300

    I did some changes in this project.

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import turtle
 
 game_screen = turtle.Screen()
 game_screen.bgcolor("red")
-game_screen.title("Turtle Game")
+game_screen.title("Eagle Game")
 
 Franklin = turtle.Turtle()
 
@@ -18,15 +18,19 @@ score_a = 0
 score_b = 0
 
 
-def turtle_swichted_red():
+def turtle_switched_red():
     Franklin.clear()
-    Franklin.write(f"You swichted Red  BLUE team: {score_b} ", align="center", font=("Courier", 24, "normal"))
+    Franklin.write(f"You Switched Red  ", align="center", font=("Courier", 24, "normal"))
 
+
+def turtle_switched_blue():
+    Franklin.clear()
+    Franklin.write(f"You Switched Blue  ", align="center", font=("Courier", 24, "normal"))
 
 def increase_score_red():
     global score_a
     score_a += 1
-    turtle_swichted_red()
+    turtle_switched_red()
     change_color_white()
     change_color_score_board_blue()
     counter_black()
@@ -36,7 +40,7 @@ def increase_score_red():
 def increase_score_blue():
     global score_b
     score_b += 1
-    turtle_scorboard()
+    turtle_switched_blue()
     change_color_black()
     change_color_score_board_red()
     counter_white()
@@ -58,18 +62,13 @@ def change_color_score_board_blue():
 def change_color_score_board_red():
     Franklin.color("red")
 
-    #turtle_scorboard()
-
-
-#increase_score_white()
-#increase_score_black()
-
 
 turtle.listen()
-turtle.onkey(fun=increase_score_red, key="w")
+turtle.onkey(fun=increase_score_red, key="r")
 turtle.onkey(fun=increase_score_blue, key="b")
 
-turtle_scorboard()
+turtle_switched_blue()
+turtle_switched_red()
 
 #counter
 
@@ -84,7 +83,7 @@ count = 0
 
 def pery_counter():
     Percy.clear()
-    Percy.write(f"Total Point: {count}", align="center", font=("Arial", 24, "normal"))
+    Percy.write(f"Total Switch: {count}", align="center", font=("Arial", 24, "normal"))
 
 
 def counter_of_percy():
@@ -135,13 +134,17 @@ game_screen.register_shape("eagle.gif.gif")  # Make sure the file is in the same
 image_path = "eagle.gif.gif"  # Replace with your GIF file name
 game_screen.bgpic(image_path)  # Set the image as the background
 
-image_left = 400  # X coordinate of the left edge of the image
-image_right = -400  # X coordinate of the right edge of the image
+image_left = -400  # X coordinate of the left edge of the image
+image_right = 400  # X coordinate of the right edge of the image
 image_bottom = -300  # Y coordinate of the bottom edge of the image
 image_top = 300  # Y coordinate of the top edge of the image
 
-
+score_display = turtle.Turtle()
+score_display.penup()
+score_display.hideturtle()
 score = 0
+
+score_display.goto(-60, 200)
 
 # Function to update the score when the image area is clicked
 def update_score(x, y):
@@ -149,8 +152,8 @@ def update_score(x, y):
     # Check if the click is within the image area
     if image_left < x < image_right and image_bottom < y < image_top:
         score += 1
-        Franklin.clear()
-        Franklin.write(f"Score: {score}", align="left", font=("Arial", 24, "normal"))
+        score_display.clear()
+        score_display.write(f"Score: {score}", align="left", font=("Arial", 24, "normal"))
 
 
 # Bind the click event on the screen
@@ -159,7 +162,8 @@ game_screen.onscreenclick(update_score)
 turtle.listen()
 
 pery_counter()
-turtle_scorboard()
+turtle_switched_blue()
+turtle_switched_red()
 increase_score_red()
 increase_score_blue()
 turtle_time()
